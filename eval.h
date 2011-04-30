63ad5eb3d4615a7f7aa97a81b12f60fd70db2839
Shawn Hyam <shawnhyam@gmail.com>
2011-04-30 13:22:01 -0400

    Added AODBM submodule; removed old versions of files.

--- eval.h
+++
@@ -1,8 +0,0 @@
-#ifndef EVAL_H_
-#define EVAL_H_
-
-#include "obj.h"
-
-any eval(any obj);
-
-#endif
--- read.h
+++
@@ -1,9 +0,0 @@
-#ifndef READ_H_
-#define READ_H_
-
-#include "obj.h"
-#include <stdio.h>
-
-any read(FILE *in);
-
-#endif

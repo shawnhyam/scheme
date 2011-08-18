069109cad2d5cbb5029dd6c759c311ac24a6ef39
Shawn Hyam <shawn@brierwooddesign.com>
2011-08-18 19:44:12 -0400

    Updates, read and write functions.

--- heap.c
+++ heap.c
@@ -4,6 +4,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <ctype.h>
 
 typedef uint64_t obj_t;
 
@@ -20,6 +21,30 @@ obj_t wrap_fixnum(fixnum_t n) { return ((obj_t)n) << 3; }
 fixnum_t unwrap_fixnum(obj_t obj) {
   assert(is_fixnum(obj));
   return (fixnum_t)(obj>>3);
+}
+
+// IMMEDIATES
+
+static const char immediate_tag = 3;
+
+char is_immediate(obj_t obj) { return (obj&7) == immediate_tag; }
+static obj_t false = immediate_tag;
+static obj_t true = (1<<3) + immediate_tag;
+
+char is_boolean(obj_t obj) { return (obj==false) || (obj==true); };
+
+// PRIMITIVE PROC
+
+typedef obj_t (*primitive_proc_t)(obj_t*);
+
+static const char primitive_proc_tag = 7;
+char is_primitive_proc(obj_t obj) { return (obj&7) == primitive_proc_tag; }
+obj_t wrap_primitive_proc(primitive_proc_t fn) {
+  assert(((obj_t)fn & 7) == 0);
+  return (obj_t)fn + primitive_proc_tag;
+}
+primitive_proc_t unwrap_primitive_proc(obj_t obj) { 
+  return (primitive_proc_t)(obj-primitive_proc_tag);
 }
 
 
@@ -169,7 +194,7 @@ struct pair * lookup(obj_t var, obj_t e) {
     e = cdr(e);
     goto nxtrib;
   } else if (unwrap_symbol(car(vars)) == unwrap_symbol(var)) {
-    return unwrap_pair(vals);
+    return unwrap_pair(car(vals));
   } else {
     vars = cdr(vars);
     vals = cdr(vals);
@@ -177,7 +202,350 @@ struct pair * lookup(obj_t var, obj_t e) {
   }
   }
 }
+/**************************** PRINT ******************************/
 
+void write(obj_t obj);
+
+void write_pair(obj_t pair) {
+    obj_t car_obj;
+    obj_t cdr_obj;
+    
+    car_obj = car(pair);
+    cdr_obj = cdr(pair);
+    write(car_obj);
+    if (is_pair(cdr_obj)) {
+        printf(" ");
+        write_pair(cdr_obj);
+    }
+    else if (is_the_empty_list(cdr_obj)) { 
+        return;
+    }
+    else {
+        printf(" . ");
+        write(cdr_obj);
+    }
+}
+
+void write(obj_t obj) {
+    char c;
+    char *str;
+    
+    if (is_the_empty_list(obj)) {
+      printf("()");
+    } else if (is_boolean(obj)) {
+      printf("#%c", obj==false ? 'f' : 't');
+    } else if (is_fixnum(obj)) {
+      printf("%lld", unwrap_fixnum(obj));
+    } else {
+      assert(0);
+    }
+#if 0
+        case SYMBOL:
+            printf("%s", obj->data.symbol->value);
+            break;
+        case FIXNUM:
+            printf("%ld", obj->data.fixnum);
+            break;
+        case CHARACTER:
+            c = obj->data.character;
+            printf("#\\");
+            switch (c) {
+                case '\n':
+                    printf("newline");
+                    break;
+                case ' ':
+                    printf("space");
+                    break;
+                default:
+                    putchar(c);
+            }
+            break;
+        case STRING:
+            str = obj->data.string->value;
+            putchar('"');
+            while (*str != '\0') {
+                switch (*str) {
+                    case '\n':
+                        printf("\\n");
+                        break;
+                    case '\\':
+                        printf("\\\\");
+                        break;
+                    case '"':
+                        printf("\\\"");
+                        break;
+                    default:
+                        putchar(*str);
+                }
+                str++;
+            }
+            putchar('"');
+            break;
+        case PAIR:
+            printf("(");
+            write_pair(obj);
+            printf(")");
+            break;
+        case PRIMITIVE_PROC:
+        case COMPOUND_PROC:
+            printf("#<procedure>");
+            break;
+        default:
+            fprintf(stderr, "cannot write unknown type\n");
+            exit(1);
+    }
+#endif
+}
+
+/***************************** READ ******************************/
+
+char is_delimiter(int c) {
+    return isspace(c) || c == EOF ||
+           c == '('   || c == ')' ||
+           c == '"'   || c == ';';
+}
+
+char is_initial(int c) {
+    return isalpha(c) || c == '*' || c == '/' || c == '>' ||
+             c == '<' || c == '=' || c == '?' || c == '!';
+}
+
+int peek(FILE *in) {
+    int c;
+
+    c = getc(in);
+    ungetc(c, in);
+    return c;
+}
+
+void eat_whitespace(FILE *in) {
+    int c;
+    
+    while ((c = getc(in)) != EOF) {
+        if (isspace(c)) {
+            continue;
+        }
+        else if (c == ';') { /* comments are whitespace also */
+            while (((c = getc(in)) != EOF) && (c != '\n'));
+            continue;
+        }
+        ungetc(c, in);
+        break;
+    }
+}
+
+void eat_expected_string(FILE *in, char *str) {
+    int c;
+
+    while (*str != '\0') {
+        c = getc(in);
+        if (c != *str) {
+            fprintf(stderr, "unexpected character '%c'\n", c);
+            exit(1);
+        }
+        str++;
+    }
+}
+
+void peek_expected_delimiter(FILE *in) {
+    if (!is_delimiter(peek(in))) {
+        fprintf(stderr, "character not followed by delimiter\n");
+        exit(1);
+    }
+}
+
+obj_t read_character(FILE *in) {
+  assert(0);
+#if 0
+    int c;
+
+    c = getc(in);
+    switch (c) {
+        case EOF:
+            fprintf(stderr, "incomplete character literal\n");
+            exit(1);
+        case 's':
+            if (peek(in) == 'p') {
+                eat_expected_string(in, "pace");
+                peek_expected_delimiter(in);
+                return make_character(' ');
+            }
+            break;
+        case 'n':
+            if (peek(in) == 'e') {
+                eat_expected_string(in, "ewline");
+                peek_expected_delimiter(in);
+                return make_character('\n');
+            }
+            break;
+    }
+    peek_expected_delimiter(in);
+    return make_character(c);
+#endif
+}
+
+obj_t read(FILE *in);
+
+obj_t read_pair(FILE *in) {
+    int c;
+    obj_t car_obj;
+    obj_t cdr_obj;
+    
+    eat_whitespace(in);
+    
+    c = getc(in);
+    if (c == ')') { /* read the empty list */
+        return the_empty_list;
+    }
+    ungetc(c, in);
+
+    car_obj = read(in);
+
+    eat_whitespace(in);
+    
+    c = getc(in);    
+    if (c == '.') { /* read improper list */
+        c = peek(in);
+        if (!is_delimiter(c)) {
+            fprintf(stderr, "dot not followed by delimiter\n");
+            exit(1);
+        }
+        cdr_obj = read(in);
+        eat_whitespace(in);
+        c = getc(in);
+        if (c != ')') {
+            fprintf(stderr,
+                    "where was the trailing right paren?\n");
+            exit(1);
+        }
+        return cons(car_obj, cdr_obj);
+    }
+    else { /* read list */
+        ungetc(c, in);
+        cdr_obj = read_pair(in);        
+        return cons(car_obj, cdr_obj);
+    }
+}
+
+obj_t read(FILE *in) {
+    int c;
+    short sign = 1;
+    int i;
+    long num = 0;
+#define BUFFER_MAX 1000
+    char buffer[BUFFER_MAX];
+
+    eat_whitespace(in);
+
+    c = getc(in);    
+
+    if (c == '#') { /* read a boolean or character */
+        c = getc(in);
+        switch (c) {
+            case 't':
+                return true;
+            case 'f':
+                return false;
+            case '\\':
+                return read_character(in);
+            default:
+                fprintf(stderr,
+                        "unknown boolean or character literal\n");
+                exit(1);
+        }
+    }
+    else if (isdigit(c) || (c == '-' && (isdigit(peek(in))))) {
+        /* read a fixnum */
+        if (c == '-') {
+            sign = -1;
+        }
+        else {
+            ungetc(c, in);
+        }
+        while (isdigit(c = getc(in))) {
+            num = (num * 10) + (c - '0');
+        }
+        num *= sign;
+        if (is_delimiter(c)) {
+            ungetc(c, in);
+            return wrap_fixnum(num);
+        }
+        else {
+            fprintf(stderr, "number not followed by delimiter\n");
+            exit(1);
+        }
+    }
+    else if (is_initial(c) ||
+             ((c == '+' || c == '-') &&
+              is_delimiter(peek(in)))) { /* read a symbol */
+        i = 0;
+        while (is_initial(c) || isdigit(c) ||
+               c == '+' || c == '-') {
+            /* subtract 1 to save space for '\0' terminator */
+            if (i < BUFFER_MAX - 1) {
+                buffer[i++] = c;
+            }
+            else {
+                fprintf(stderr, "symbol too long. "
+                        "Maximum length is %d\n", BUFFER_MAX);
+                exit(1);
+            }
+            c = getc(in);
+        }
+        if (is_delimiter(c)) {
+            buffer[i] = '\0';
+            ungetc(c, in);
+            return cons_symbol(buffer);
+        }
+        else {
+            fprintf(stderr, "symbol not followed by delimiter. "
+                            "Found '%c'\n", c);
+            exit(1);
+        }
+    }
+    else if (c == '"') { /* read a string */
+        i = 0;
+        while ((c = getc(in)) != '"') {
+            if (c == '\\') {
+                c = getc(in);
+                if (c == 'n') {
+                    c = '\n';
+                }
+            }
+            if (c == EOF) {
+                fprintf(stderr, "non-terminated string literal\n");
+                exit(1);
+            }
+            /* subtract 1 to save space for '\0' terminator */
+            if (i < BUFFER_MAX - 1) {
+                buffer[i++] = c;
+            }
+            else {
+                fprintf(stderr, 
+                        "string too long. Maximum length is %d\n",
+                        BUFFER_MAX);
+                exit(1);
+            }
+        }
+        buffer[i] = '\0';
+        assert(0);
+        // FIXME        return make_string(buffer);
+    }
+    else if (c == '(') { /* read the empty list or pair */
+        return read_pair(in);
+    }
+    else if (c == '\'') { /* read quoted expression */
+      return cons(cons_symbol("quote"), cons(read(in), the_empty_list));
+    }
+    else {
+        fprintf(stderr, "bad input. Unexpected '%c'\n", c);
+        exit(1);
+    }
+    fprintf(stderr, "read illegal state\n");
+    exit(1);
+}
+
+//////
 
 char is_tagged_list(obj_t expr, struct symbol *tag) {
   if (is_pair(expr)) {
@@ -236,7 +604,6 @@ obj_t compile(obj_t x, obj_t next) {
       obj_t test = cadr(x);
       obj_t on_true = caddr(x);
       obj_t on_false = cadddr(x);
-      printf("%llu %llu", on_true, on_false);
       obj_t truec = compile(on_true, next);
       obj_t falsec = compile(on_false, next);
       return compile(test, list3(cons_symbol("test"), truec, falsec));
@@ -272,13 +639,14 @@ obj_t extend(obj_t e, obj_t vars, obj_t vals) {
 }
 
 obj_t VM(obj_t a, obj_t x, obj_t e, obj_t r, obj_t s) {
-  //printf("%llu", x);
   printf("%s\n", unwrap_symbol(car(x))->value);
 
   if (is_tagged_list(x, make_symbol("halt"))) {
     return a;
   } else if (is_tagged_list(x, make_symbol("refer"))) {
-    return VM(lookup(cadr(x), e)->car, caddr(x), e, r, s);
+    obj_t val = lookup(cadr(x), e)->car;
+    printf("%lld", val);
+    return VM(val, caddr(x), e, r, s);
   } else if (is_tagged_list(x, make_symbol("constant"))) {
     return VM(cadr(x), caddr(x), e, r, s);
   } else if (is_tagged_list(x, make_symbol("close"))) {
@@ -287,7 +655,7 @@ obj_t VM(obj_t a, obj_t x, obj_t e, obj_t r, obj_t s) {
     obj_t x_ = cadddr(x);
     return VM(closure(body, e, vars), x_, e, r, s);
   } else if (is_tagged_list(x, make_symbol("test"))) {
-    return VM(a, a ? cadr(x) : caddr(x), e, r, s);
+    return VM(a, (a!=false) ? cadr(x) : caddr(x), e, r, s);
   } else if (is_tagged_list(x, make_symbol("frame"))) {
     return VM(a, caddr(x), e, the_empty_list, call_frame(cadr(x), e, r, s));
   } else if (is_tagged_list(x, make_symbol("argument"))) {
@@ -304,10 +672,15 @@ obj_t VM(obj_t a, obj_t x, obj_t e, obj_t r, obj_t s) {
   }
 } 
 
+
 obj_t eval(obj_t x) {
+  //  obj_t fn = make_primitive_proc(is_null_proc);
+
+  obj_t globals = list2(list1(cons_symbol("x")), list1(wrap_fixnum(44)));
+
   return VM(the_empty_list, 
 	    compile(x, list1(cons_symbol("halt"))),
-	    the_empty_list,
+	    list1(globals),
 	    the_empty_list,
 	    the_empty_list);
 }
@@ -328,8 +701,12 @@ int main() {
 		     wrap_fixnum(4));
 #endif
 
-  obj_t a = eval(expr);
-  printf("%d\n", unwrap_fixnum(a));
+  while (1) {
+    printf("\n> ");
+    write(eval(read(stdin)));
+  }
+
+  //obj_t a = eval(expr);
 
 
 }
